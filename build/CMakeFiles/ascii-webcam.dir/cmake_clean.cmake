44d99e7a37eae53f820d4ec2d69370c3feef6bc9
Arnaud <arnaud.guerout@epitech.eu>
2021-08-01 13:24:18 +0200

    [FIX] OBS issue (idk how the fuck), still issue with google and skype

---
+++ build/CMakeFiles/ascii-webcam.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ascii-webcam.dir/src/common/ArgParser.cpp.o"
+  "CMakeFiles/ascii-webcam.dir/src/common/ArgParser.cpp.o.d"
+  "CMakeFiles/ascii-webcam.dir/src/common/AsciiProcessor.cpp.o"
+  "CMakeFiles/ascii-webcam.dir/src/common/AsciiProcessor.cpp.o.d"
+  "CMakeFiles/ascii-webcam.dir/src/linux/VideoCapture.cpp.o"
+  "CMakeFiles/ascii-webcam.dir/src/linux/VideoCapture.cpp.o.d"
+  "CMakeFiles/ascii-webcam.dir/src/linux/VideoOutput.cpp.o"
+  "CMakeFiles/ascii-webcam.dir/src/linux/VideoOutput.cpp.o.d"
+  "CMakeFiles/ascii-webcam.dir/src/main.cpp.o"
+  "CMakeFiles/ascii-webcam.dir/src/main.cpp.o.d"
+  "ascii-webcam"
+  "ascii-webcam.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ascii-webcam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/ascii-webcam.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ascii-webcam.
+# This may be replaced when dependencies are built.
